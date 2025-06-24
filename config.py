a8faa575665ef1ba324fa9332f73ca1d6f5f0f97
Aziz HK <zizou.hajkacem@gmail.com>
2025-06-24 16:37:45 +0100

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import streamlit as st
+#import streamlit as st
 # --- Continuous Scraping Configuration ---
 SCRAPING_INTERVAL_MINUTES = 15 # The time in minutes between each scrape
 
 # OpenAI API Key
-OPENAI_API_KEY = "YOUR_OPENAI_API_KEY_HERE"
+#OPENAI_API_KEY = "YOUR_OPENAI_API_KEY_HERE"
 
 # --- Supabase Configuration ---
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+#SUPABASE_URL = st.secrets["SUPABASE_URL"]
+#SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
